3ec11234d8f7552e3c73604fdfde44d11af3b13f
Becheru888 <becheru93@gmail.com>
2019-06-07 09:55:17 +0100

    creating tabs components with map done

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Tab from './Tab';
+import uuid from "uuid";
+
+
 const Tabs = props => {
   return (
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+        {props.tabs.map(tab=>(
+          <Tab selectedTab ={props.selectedtab} selectTabhandler={props.selectTabHandler} tab ={tab} key={uuid()}/>
+        ))}
       </div>
     </div>
   );
