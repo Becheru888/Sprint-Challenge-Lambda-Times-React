42dfeaf92be0b28615188ea4fe7b7f4da8f41f95
Becheru888 <becheru93@gmail.com>
2019-06-14 08:40:27 +0100

    App refactored TimesPage comp created

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-import TopBar from './components/TopBar';
-import Header from './components/Header';
-import Content from './components/Content/Content';
+import React from 'react';
+import TimesPage from './components/TimesPage/TimesPage'
 
 const App = () => {
   return (
     <div className="App">
-      <TopBar />
-      <Header />
-      <Content />
+     <TimesPage/>
     </div>
   );
 }
